841ec8b8ffc5d2808a89a0bbdd1fea80c25f0232
steingartpeter <steingartpeter@users.noreply.github.com>
2021-03-05 06:42:51 +0100

    2021-03-05 VFOR/VIF 540XS Linux

--- VUE_101/app.js
+++ VUE_101/app.js
@@ -2,12 +2,39 @@ const app = Vue.createApp({
   data() {
     return {
       enteredGoal: "",
-      goals: [],
+      goals: [
+        {
+          id: 0,
+          description: "Megoldani a v-for feladatot",
+          solved: false,
+        },
+        {
+          id: 1,
+          description: "Végigvinni a kurzust",
+          solved: false,
+        },
+        {
+          id: 2,
+          description: "Játékot csinálni VUe-vel",
+          solved: false,
+        },
+      ],
     };
   },
   methods: {
     addGoal() {
-      this.goals.push(this.enteredGoal);
+      console.log(this.goals);
+      let nextId = this.goals[this.goals.length - 1].id + 1;
+      let dscr = this.enteredGoal;
+      let newGoal = {
+        id: nextId,
+        description: dscr,
+        solved: false,
+      };
+      this.goals.push(newGoal);
+    },
+    removeGoal(idx) {
+      this.goals.splice(idx, 1);
     },
   },
 });
