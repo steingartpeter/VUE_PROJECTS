a55fa20231a912774291b8efa404b407ee19cfd8
steingartpeter <steingartp.2@gmail.com>
2021-03-07 18:05:17 +0100

    2021-03-07 001 X550J Win10

--- ASSGN_004/app.js
+++ ASSGN_004/app.js
@@ -2,8 +2,30 @@ const app = Vue.createApp({
   data() {
     return {
       tstMsg: "OK, mount works!",
+      inpTask: "",
+      tasks: [],
+      is_lstShown: true,
+      btnCaption: "Hide List",
     };
   },
-  methods: {},
+  methods: {
+    addTask() {
+      let cntr = this.tasks.length;
+      let new_task = {
+        task_id: cntr,
+        description: this.inpTask,
+      };
+      this.tasks.push(new_task);
+      this.inpTask = "";
+    },
+    toggleShowList() {
+      this.is_lstShown = !this.is_lstShown;
+      if (this.is_lstShown) {
+        this.btnCaption = "Hide List!";
+      } else {
+        this.btnCaption = "Show List!";
+      }
+    },
+  },
 });
 app.mount("#assignment");
