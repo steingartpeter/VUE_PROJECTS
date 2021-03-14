4fe169771adffd0fb671394c5cfa68d1f6d40eb3
steingartpeter <steingart.p2@gmail.com>
2021-03-14 12:49:20 +0100

    2021-03-15 004 PAKS2

---
+++ VUE_702/src/components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails()">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>E-mail:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "Manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 456 7890",
+        email: "manLrnz@yahoo.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
