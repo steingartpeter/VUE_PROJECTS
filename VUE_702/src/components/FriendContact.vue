d54d3f706479e7b2ac71282a23616d80bb896092
steingartpeter <steingartp.2@gmail.com>
2021-03-21 11:08:39 +0100

    2021-03-21 001 X550J

--- VUE_702/src/components/FriendContact.vue
+++ VUE_702/src/components/FriendContact.vue
@@ -13,50 +13,62 @@
 </template>
 
 <script>
-export default {
-  //props: ["name", "phone", "email", "isFavorite"],
-  props: {
-    id: { type: String, required: true },
-    name: { type: String, required: true },
-    phone: { type: String, required: true },
-    email: { type: String, required: true },
-    isFavorite: {
-      type: Boolean,
-      required: false,
-      default: false,
-      // validator: function(value) {
-      //   return value === "1" || value === "0";
-      // },
+  export default {
+    //props: ["name", "phone", "email", "isFavorite"],
+    props: {
+      id: { type: String, required: true },
+      name: { type: String, required: true },
+      phone: { type: String, required: true },
+      email: { type: String, required: true },
+      isFavorite: {
+        type: Boolean,
+        required: false,
+        default: false,
+        // validator: function(value) {
+        //   return value === "1" || value === "0";
+        // },
+      },
     },
-  },
-  data() {
-    return {
-      detailsAreVisible: false,
-      // friend: {
-      //   id: "Manuel",
-      //   name: "Manuel Lorenz",
-      //   phone: "0123 456 7890",
-      //   email: "manLrnz@yahoo.com",
-      // },
-      //friendIsFvorite: this.isFavorite,
-    };
-  },
-  methods: {
-    toggleDetails() {
-      this.detailsAreVisible = !this.detailsAreVisible;
+    emits: ["toggleFavorite"],
+    // emits: {
+    //   toggleFavorite: function(id) {
+    //     //validation
+    //     if (id) {
+    //       return true;
+    //     } else {
+    //       console.warn("ID is missing!");
+    //       return false;
+    //     }
+    //   },
+    // },
+    data() {
+      return {
+        detailsAreVisible: false,
+        // friend: {
+        //   id: "Manuel",
+        //   name: "Manuel Lorenz",
+        //   phone: "0123 456 7890",
+        //   email: "manLrnz@yahoo.com",
+        // },
+        //friendIsFvorite: this.isFavorite,
+      };
     },
-    toggleFavorite() {
-      //this.friendIsFvorite = !this.friendIsFvorite;
-      this.$emit("tgl-isfav", this.id);
+    methods: {
+      toggleDetails() {
+        this.detailsAreVisible = !this.detailsAreVisible;
+      },
+      toggleFavorite() {
+        //this.friendIsFvorite = !this.friendIsFvorite;
+        this.$emit("tgl-isfav", this.id);
+      },
     },
-  },
-};
+  };
 </script>
 
 <style scoped>
-button {
-  box-shadow: 3px 3px 6px #050505;
-  margin-left: 5px;
-  border-bottom: dotted 1px #050505 !important;
-}
+  button {
+    box-shadow: 3px 3px 6px #050505;
+    margin-left: 5px;
+    border-bottom: dotted 1px #050505 !important;
+  }
 </style>
