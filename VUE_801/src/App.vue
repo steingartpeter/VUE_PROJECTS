511b7fe1e7b5d9398919e77b72a3f88010d3c180
steingartpeter <steingartp.2@gmail.com>
2021-03-25 19:27:23 +0100

    002 2021-03-25 X550J

--- VUE_801/src/App.vue
+++ VUE_801/src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
+    <button @click="setSelectedComponent('active-goals')">Active goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage goals</button>
+    <!--
+      OLD SCHOOL V-IF METHOD     
+    <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> 
+    Instead of this use dynamic components, with Vue defined <component> tag
+    -->
+    <component :is="selectedComponent"></component>
+
+    <!--
+      SLOT PRACTICE 
+      <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
@@ -12,30 +24,43 @@
         <h2>{{ slotProp.goal }}</h2>
         <p>{{ slotProp.nextPropFromSlot }}</p>
       </template>
-    </course-goals>
+    </course-goals> -->
   </div>
 </template>
 
 <script>
   import TheHeader from "./components/TheHeader.vue";
-  import UserInfo from "./components/UserInfo";
-  import BadgeList from "./components/BadgeList";
-  import CourseGoals from "./components/CourseGoals";
+  // import UserInfo from "./components/UserInfo";
+  // import BadgeList from "./components/BadgeList";
+  // import CourseGoals from "./components/CourseGoals";
+  import ActiveGoals from "./components/ActiveGoals";
+  import ManageGoals from "./components/ManageGoals";
+  //import ActiveElement from "../../A_POTENTIAL_PROBLEM/src/components/ActiveElement.vue";
+
   export default {
     components: {
       "the-header": TheHeader,
-      "user-info": UserInfo,
-      "badge-list": BadgeList,
-      "course-goals": CourseGoals,
+      // "user-info": UserInfo,
+      // "badge-list": BadgeList,
+      // "course-goals": CourseGoals,
+      "active-goals": ActiveGoals,
+      "manage-goals": ManageGoals,
+      //ActiveElement,
     },
     data() {
       return {
+        selectedComponent: "active-goals",
         activeUser: {
           name: "Maximilian Schwarzmüller",
           description: "Site owner and admin",
           role: "admin",
         },
       };
+    },
+    methods: {
+      setSelectedComponent(cmp) {
+        this.selectedComponent = cmp;
+      },
     },
   };
 </script>
