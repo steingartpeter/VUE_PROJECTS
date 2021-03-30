196b010b4aa9a4259a7db3b5c5d357eba9fa02ae
steingartpeter <steingartp.2@gmail.com>
2021-03-30 19:48:29 +0200

    2021-03-30 001 X550J

--- VUE_901/src/App.vue
+++ VUE_901/src/App.vue
@@ -1,13 +1,49 @@
-<template> <ul></ul> </template>
+<template>
+  <div>
+    <h2>Learning resources</h2>
+    <stored-resources :resources="storedResources"></stored-resources>
+  </div>
+</template>
 
 <script>
+import StoredResources from './components/learning-resources/storedResources';
 export default {
+  components: {
+    StoredResources
+  },
   data() {
     return {
-      storedResources: []
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'Officila Vue-js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'GOOGLE',
+          description: 'Learn to google...',
+          link: 'https://www.google.com'
+        }
+      ]
     };
   }
 };
 </script>
 
-<style></style>
+<style>
+/* ul {
+  background-color: burlywood;
+} */
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: 'Roboto', sans-serif;
+}
+body {
+  margin: 0;
+}
+</style>
